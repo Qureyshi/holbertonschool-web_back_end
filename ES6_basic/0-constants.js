d5dd2fcc4ab2e9b986e72be81983633ec9cf6dfa
Qureyshi <kamranqureyshi1998@gmail.com>
2024-07-19 12:07:57 +0000

    js

--- ES6_basic/0-constants.js
+++ ES6_basic/0-constants.js
@@ -1,12 +1,8 @@
-// 0-constants.js
-
-// Function to use const for instantiating variables
 export function taskFirst() {
   const task = 'I prefer const when I can.';
   return task;
 }
 
-// Function to use let for instantiating variables
 export function taskNext() {
   let combination = 'But sometimes let';
   combination += getLast();
@@ -14,7 +10,6 @@ export function taskNext() {
   return combination;
 }
 
-// Helper function to be used in taskNext
 export function getLast() {
   return ' is okay';
 }
