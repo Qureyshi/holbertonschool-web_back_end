364c2d90be1c5c26a93007b4f94904c685e88611
Qureyshi <kamranqureyshi1998@gmail.com>
2024-07-19 19:19:38 +0000

    js

--- ES6_classes/4-pricing.js
+++ ES6_classes/4-pricing.js
@@ -1,50 +1,44 @@
-import Currency from './3-currency.js';
+import Currency from './3-currency';
 
 export default class Pricing {
   constructor(amount, currency) {
-    this._amount = amount;
-    this._currency = currency;
+    this.amount = amount;
+    this.currency = currency;
   }
 
-  // Getter for amount
   get amount() {
     return this._amount;
   }
 
-  // Setter for amount
-  set amount(newAmount) {
-    if (typeof newAmount === 'number') {
-      this._amount = newAmount;
-    } else {
+  set amount(amount) {
+    if (typeof amount !== 'number') {
       throw new TypeError('Amount must be a number');
     }
+
+    this._amount = amount;
   }
 
-  // Getter for currency
   get currency() {
     return this._currency;
   }
 
-  // Setter for currency
-  set currency(newCurrency) {
-    if (newCurrency instanceof Currency) {
-      this._currency = newCurrency;
-    } else {
-      throw new TypeError('Currency must be an instance of Currency');
+  set currency(currency) {
+    if (!(currency instanceof Currency)) {
+      throw new TypeError('Currency must be an instance of Currency class');
     }
+
+    this._currency = currency;
   }
 
-  // Method to display full price
   displayFullPrice() {
-    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+    return `${this._amount} ${this._currency.displayFullCurrency()}`;
   }
 
-  // Static method to convert price
   static convertPrice(amount, conversionRate) {
-    if (typeof amount === 'number' && typeof conversionRate === 'number') {
-      return amount * conversionRate;
-    } else {
-      throw new TypeError('Both amount and conversionRate must be numbers');
+    if (typeof amount !== 'number' || typeof conversionRate !== 'number') {
+      throw new TypeError('Converting price: values must be a number');
     }
+
+    return amount * conversionRate;
   }
 }
