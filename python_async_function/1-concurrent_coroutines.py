0eee26d1c8ef9cb6564919237b3c651231aef68d
Qureyshi <159009151+Qureyshi@users.noreply.github.com>
2024-08-16 22:04:45 +0300

    Update 1-concurrent_coroutines.py

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+"""
+Module
+"""
 from typing import List
 
 
@@ -6,6 +9,9 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    function
+    """
     delays = []
     for i in range(n):
         delays.append(await wait_random(max_delay))
