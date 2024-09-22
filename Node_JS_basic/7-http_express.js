3268441269ab885afaf9b3b0990f6a9be3cc23d7
Qureyshi <kamranqureyshi1998@gmail.com>
2024-09-22 16:09:05 +0300

    node.js

--- Node_JS_basic/7-http_express.js
+++ Node_JS_basic/7-http_express.js
@@ -1,24 +1,20 @@
 const express = require('express');
 const countStudents = require('./3-read_file_async');
 
-const database = process.argv[2];
-const port = 1245;
 const app = express();
-module.exports = app;
+const port = 1245;
+const hostname = '127.0.0.1';
+const filePath = process.argv[2];
+const promise = countStudents(filePath);
 
 app.get('/', (req, res) => {
-  res.setHeader('Content-Type', 'text/plain');
   res.send('Hello Holberton School!');
 });
 
-app.get('/students', (req, res) => {
-  res.setHeader('Content-Type', 'text/plain');
-  res.write('This is the list of our students\n');
-  countStudents(database).then((data) => {
-    res.end(data.join('\n'));
-  }).catch((error) => {
-    res.end(`${error.message}`);
-  });
+app.get('/students', (req, rep) => {
+  promise.then((data) => {
+    rep.send(`This is the list of our students\n${data}`);
+  }).catch(() => { rep.send('This is the list of our students\nCannot load the database'); });
 });
 
-app.listen(port);
+module.exports = app.listen(port, hostname);
